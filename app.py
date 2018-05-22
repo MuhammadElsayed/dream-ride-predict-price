f510a525f5014483e9efae539cf8549a0878266f
Sumit <sumit.wankhede@hotmail.com>
2018-05-21 22:28:22 -0500

    All correction

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, abort, jsonify, request, render_template
 from sklearn.externals import joblib
 import numpy as np
 import json
+import pandas as pd
 
-gbr = joblib.load('model.pkl')
+gbr = joblib.load('modelxgb.pkl')
 
 app = Flask(__name__)
 
@@ -24,76 +25,74 @@ def home():
 
 def input_to_one_hot(data):
     # initialize the target vector with zero values
-    enc_input = np.zeros(61)
+    enc_input = np.zeros(4)
     # set the numerical input as they are
-    enc_input[0] = data['year_model']
-    enc_input[1] = data['mileage']
-    enc_input[2] = data['fiscal_power']
+    enc_input[0] = '2006'
+    enc_input[1] = '150000'
+    enc_input[2] = '25639'
+    enc_input[3] = '29771'
+
+    #enc_input[0] = data['year_model']
+    #enc_input[1] = data['mileage']
+    #enc_input[2] = data['make']
+    #enc_input[3] = data['model']
+
+    #enc_input[2] = data['fiscal_power']
     ##################### Mark #########################
-    # get the array of marks categories
-    marks = ['Peugeot', 'Renault', 'Citroen', 'Mercedes-Benz', 'Ford', 'Nissan',
-       'Fiat', 'Skoda', 'Hyundai', 'Kia', 'Dacia', 'Opel', 'Volkswagen',
-       'mini', 'Seat', 'Isuzu', 'Honda', 'Mitsubishi', 'Toyota', 'BMW',
-       'Chevrolet', 'Audi', 'Suzuki', 'Ssangyong', 'lancia', 'Jaguar',
-       'Volvo', 'Autres', 'BYD', 'Daihatsu', 'Land Rover', 'Jeep', 'Chery',
-       'Alfa Romeo', 'Bentley', 'Daewoo', 'Hummer', 'Mazda', 'Chrysler',
-       'Maserati', 'Cadillac', 'Dodge', 'Rover', 'Porsche', 'GMC',
-       'Infiniti', 'Changhe', 'Geely', 'Zotye', 'UFO', 'Foton', 'Pontiac',
-       'Acura', 'Lexus']
-    cols = ['year_model', 'mileage', 'fiscal_power', 'fuel_type_Diesel',
-       'fuel_type_Electrique', 'fuel_type_Essence', 'fuel_type_LPG',
-       'mark_Acura', 'mark_Alfa Romeo', 'mark_Audi', 'mark_Autres', 'mark_BMW',
-       'mark_BYD', 'mark_Bentley', 'mark_Cadillac', 'mark_Changhe',
-       'mark_Chery', 'mark_Chevrolet', 'mark_Chrysler', 'mark_Citroen',
-       'mark_Dacia', 'mark_Daewoo', 'mark_Daihatsu', 'mark_Dodge', 'mark_Fiat',
-       'mark_Ford', 'mark_Foton', 'mark_GMC', 'mark_Geely', 'mark_Honda',
-       'mark_Hummer', 'mark_Hyundai', 'mark_Infiniti', 'mark_Isuzu',
-       'mark_Jaguar', 'mark_Jeep', 'mark_Kia', 'mark_Land Rover', 'mark_Lexus',
-       'mark_Maserati', 'mark_Mazda', 'mark_Mercedes-Benz', 'mark_Mitsubishi',
-       'mark_Nissan', 'mark_Opel', 'mark_Peugeot', 'mark_Pontiac',
-       'mark_Porsche', 'mark_Renault', 'mark_Rover', 'mark_Seat', 'mark_Skoda',
-       'mark_Ssangyong', 'mark_Suzuki', 'mark_Toyota', 'mark_UFO',
-       'mark_Volkswagen', 'mark_Volvo', 'mark_Zotye', 'mark_lancia',
-       'mark_mini']
+    # # get the array of marks categories
+    # marks = ['Acura', 'Alfa', 'AM', 'Aston', 'Audi', 'Bentley', 'BMW', 'Buick', 'Cadillac', 'Chevrolet', 'Chrysler',
+    #          'Dodge', 'FIAT', 'Fisker', 'Ford', 'Freightliner', 'Genesis', 'Geo', 'GMC', 'Honda', 'HUMMER', 'Hyundai',
+    #          'INFINITI', 'Isuzu', 'Jaguar', 'Jeep', 'Kia', 'Land', 'Lexus', 'Lincoln', 'Lotus', 'Maserati', 'Mazda',
+    #          'Mercedes-Benz', 'Mercury', 'MINI', 'Mitsubishi', 'Nissan', 'Oldsmobile', 'Plymouth', 'Pontiac',
+    #          'Porsche', 'Ram', 'Saab', 'Saturn', 'Scion', 'smart', 'Subaru', 'Suzuki', 'Tesla', 'Toyota', 'Volkswagen',
+    #          'Volvo']
+    #cols = ['year_model', 'mileage', 'model', 'make']
 
     # redefine the the user inout to match the column name
-    redefinded_user_input = 'mark_'+data['mark']
+    #redefinded_user_input = 'mark_'+data['mark']
     # search for the index in columns name list 
-    mark_column_index = cols.index(redefinded_user_input)
+    #mark_column_index = cols.index(redefinded_user_input)
     #print(mark_column_index)
     # fullfill the found index with 1
-    enc_input[mark_column_index] = 1
+    #enc_input[mark_column_index] = 1
     ##################### Fuel Type ####################
     # get the array of fuel type
-    fuel_type = ['Diesel', 'Essence', 'Electrique', 'LPG']
+    # fuel_type = ['Diesel', 'Essence', 'Electrique', 'LPG']
     # redefine the the user inout to match the column name
-    redefinded_user_input = 'fuel_type_'+data['fuel_type']
+    # redefinded_user_input = 'fuel_type_'+data['fuel_type']
     # search for the index in columns name list 
-    fuelType_column_index = cols.index(redefinded_user_input)
+    # fuelType_column_index = cols.index(redefinded_user_input)
     # fullfill the found index with 1
-    enc_input[fuelType_column_index] = 1
+    # enc_input[fuelType_column_index] = 1
     return enc_input
 
 @app.route('/api',methods=['POST'])
 def get_delay():
-    result=request.form
+    result = request.form
     year_model = result['year_model']
     mileage = result['mileage']
     make = result['mark']
     model = result['model']
 
-    user_input = {'year_model':year_model, 'mileage':mileage, 'model':model, 'make':make}
-    
-    print(user_input)
+    #user_input = {'Year': year_model, 'Mileage': mileage, 'MakeNum': model, 'StateNum': '0', 'ModelNum': make}
+    #user_input = {'Year': year_model, 'Mileage': mileage, 'MakeNum': '25639', 'ModelNum': '29771'}
+
+    #print(user_input)
 
     #a = input_to_one_hot(user_input)
 
+    df = pd.DataFrame(columns=['Year', 'Mileage', 'MakeNum', 'ModelNum'], index=['0'])
+    df.loc['0'] = pd.Series({'Year': year_model, 'Mileage': mileage, 'MakeNum': make, 'ModelNum': model})
 
-    price_pred = gbr.predict([user_input])[0]
-    #price_pred = round(price_pred, 2)
-    return json.dumps({'price': price_pred});
+    #df.loc['0'] = pd.Series({'Year': 2015, 'Mileage': 50000, 'MakeNum': 25639, 'ModelNum': 29771})
+    #y = gbr.predict(df)
 
-    #return json.dumps({'price $500'});
+    price_pred = gbr.predict(df)
+
+    price_pred = np.exp(price_pred)
+    #price_pred = round(price_pred,2)
+    return json.dumps({'price': round(price_pred[0],2)});
+
     # return render_template('result.html',prediction=price_pred)
 
 if __name__ == '__main__':
